ab2aa73cfaf266194a50f677f7cb309625b2a667
khomrachsof <sayoshii@mail.ru>
2025-04-17 11:10:16 +0300

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,13 @@
+   #ifndef CALCULATOR_H
+  #define CALCULATOR_H
+
+  class Calculator {
+  public:
+      static int add(int a, int b);
+      static int subtract(int a, int b);
+      static int multiply(int a, int b);
+      static double divide(int a, int b);
+  };
+
+  #endif
+  
